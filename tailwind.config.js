885a14635093d8619087432d586f802577175c6b
rory <rorymalone@live.com>
2024-01-24 00:58:52 +0000

    Added themes and default styles to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -10,11 +12,30 @@ export default {
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
+        fontFamily: {
+            primary: "Playfair Display",
+            body: "Work Sans"
         },
+        container:{
+            default: "1rem",
+            lg: "3rem"
+        },
+        extend:{
+            colors:{
+                "light-primary": "#E3FDFD",
+                "light-secondary": "#CBF1F5",
+                "light-tertiary": "#A6E3E9",
+                "light-quaternary": "#71C9CE",
+                "dark-primary": "#222831",
+                "dark-secondary": "#393E46",
+                "dark-tertiary": "#29ADB5",
+                "dark-quaternary": "#71C9CE",
+                blue : colors.blue,
+                indigo: colors.indigo,
+                green: colors.green,
+                red: colors.red
+            }
+        }
     },
 
     plugins: [forms],
