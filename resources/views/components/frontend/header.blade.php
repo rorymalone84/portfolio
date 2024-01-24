19ebfbc632b30fd2d02f5f159010f2222afaa263
rory <rorymalone@live.com>
2024-01-24 02:45:25 +0000

    Responsive navbar fix

--- resources/views/components/frontend/header.blade.php
+++ resources/views/components/frontend/header.blade.php
@@ -1,11 +1,9 @@
-<header x-data="{ open: false }">
+<header x-data="{ mobileNav: false }">
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </a>
-            <button x-on:click="open = ! open" data-collapse-toggle="navbar-default" type="button"
+            <x-application-logo />
+
+            <button x-on:click="mobileNav = ! mobileNav" data-collapse-toggle="navbar-default" type="button"
                 class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 aria-controls="navbar-default" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
@@ -15,7 +13,7 @@
                         d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
             </button>
-            <div x-show="open" class="w-full md:block md:w-auto">
+            <div :class="mobileNav ? '' : 'hidden'" class=" w-full md:block md:w-auto">
                 <ul
                     class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
